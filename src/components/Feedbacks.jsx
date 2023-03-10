6207de4237e142c2ff001e3f89901ba37ca30b39
Saidur Rahman <saidurrahman1473@gmail.com>
2023-03-10 10:53:28 +0600

    fixing nav

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Feedbacks = () => {
     return (
         <div>
-            Experience
+            feedback
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbnar
-        </div>
-    );
+  const [active, setActive] = useState("");
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex item-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+
+          <div className="text-white text-[18px] font-bold cursor-pointer">
+            Saidur Rahman
+            <span className="sm:block hidden">| MERN Stack Dev.</span>
+          </div>
+        </Link>
+
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+
+            {navLinks.map((link)=>{
+                <li>
+                    <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+            })}
+          </ul>
+
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
